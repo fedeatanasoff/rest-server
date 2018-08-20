afb9669c943a2390d95ac93466a654361a0748a7
fd <fede.atanasoff@gmail.com>
2018-08-20 01:12:47 -0300

    integrando unique validator para el role y email

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
 
 let Schema = mongoose.Schema;
+let rolesValidos = {
+  values: ["ADMIN_ROLE", "USER_ROLE"],
+  message: "{VALUE} no es un rol valido"
+};
 
 let usuarioSchema = new Schema({
   nombre: {
@@ -9,6 +14,7 @@ let usuarioSchema = new Schema({
   },
   email: {
     type: String,
+    unique: true,
     required: [true, "El correo es necesario"]
   },
   password: {
@@ -21,7 +27,8 @@ let usuarioSchema = new Schema({
   },
   role: {
     type: String,
-    default: "USER_ROLE"
+    default: "USER_ROLE",
+    enum: rolesValidos
   },
   estado: {
     type: Boolean,
@@ -32,5 +39,7 @@ let usuarioSchema = new Schema({
     default: false
   }
 });
+
+usuarioSchema.plugin(uniqueValidator, { message: "{PATH} debe de ser unico" });
 
 module.exports = mongoose.model("Usuario", usuarioSchema);
